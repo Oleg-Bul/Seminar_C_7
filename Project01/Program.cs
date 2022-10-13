4a4733fa3ed23c04ed3733068a6c38857ff19a8b
Oleg_bul <kokseg63@gmail.com>
2022-10-13 23:02:37 +0400

    fin

--- Project01/Program.cs
+++ Project01/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 60. ...Сформируйте трёхмерный массив 
+//из неповторяющихся  чисел. Напишите программу, 
+//которая будет построчно выводить массив, 
+//добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+using System.Linq;
+
+bool FindElement(int element, int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                if(array[i,j,z] == element) return true;
+            }
+        }
+    }
+    return false;
+}
+// 3 5 6
+int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+int[,,] baseArray = new int[size[0],size[1],size[2]];
+for (int i = 0; i < baseArray.GetLength(0); i++)
+{
+    for (int j = 0; j < baseArray.GetLength(1); j++)
+    {
+        int k = 0;
+        while (k < baseArray.GetLength(2))
+        {
+            int rnd = new Random().Next(10, 1000);
+            if(!FindElement(rnd,baseArray))
+            {
+                baseArray[i,j,k] = rnd;
+                k++;
+            }
+        }
+    }
+    
+}
+PrintArray(baseArray);
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Project02/Program.cs
+++ Project02/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход 
+// принимает два числа A и B,
+//  и возводит число А в целую степень B 
+//  с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+
+// A = 2; B = 3 -> 8
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+     int number = int.Parse(System.Console.ReadLine()??"0");
+    return number;
+}
+int a = ReadData("Ввведите размер число А ");
+int b = ReadData("Введите размер число Б ");
+int PowAtoB(int a, int b)
+{   
+    int sum = a;
+    for ( int i = 1; i < b;i++)
+    {
+       sum = a*a;
+       b++;
+    }return sum;
+    
+    
+}
+
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg+value);
+}
+
+PrintData(" Число A в степени B равно : ", PowAtoB(a,b));
+
+
+//https://t.me/VladVin2314
